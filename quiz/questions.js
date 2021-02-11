54ceea3961a397f7be68ab98cb698169091ba53a
Manoj-eng <manojtewari168@gmail.com>
2021-02-11 17:05:38 +0530

    manoj

---
+++ quiz/questions.js
@@ -0,0 +1,16 @@
+var questions = [
+  ["who won the cricket world cup final of 2019?" , "India" , "NewZeland", "South Africa" , "England", "4"],
+  ["Who is the most followed cricketer in Instagram" , "Virat kohli", "steve smith", "Joe Root", "kane williamson", "1" ],
+  ["Which player has most number of centuries in the world", "Sachin Tendulkar","Brian Lara", "Jacques kallis", "Ricky ponting","1"],
+  ["Which Indian player has taken 10 wickets in an innings?", "Irfan pathan" , "Zaheer khan", "harbhajan singh","Anil kumble","4"],
+  ["Which team has won the world cup final 2011", "Australia","South Africa","India","Pakistan","3"],
+  ["India won their first world cup in which year?","1983","1987","2011","2003", "1"],
+  ["Which team has won most number of world cup titles","India", "Australia","Zimbabwe","England", "2"],
+  ["Who is the most followed cricketer in Instagram" , "Virat kohli", "steve smith", "Joe Root", "kane williamson", "1" ],
+  ["Which player has most number of centuries in the world", "Sachin Tendulkar","Brian Lara", "Jacques kallis", "Ricky ponting","1"],
+  ["Which Indian player has taken 10 wickets in an innings?", "Irfan pathan" , "Zaheer khan", "harbhajan singh","Anil kumble","4"],
+  ["Which team has won the world cup final 2011", "Australia","South Africa","India","Pakistan","3"],
+  ["India won their first world cup in which year?","1983","1987","2011","2003", "1"],
+  ["Which team has won most number of world cup titles","India", "Australia","Zimbabwe","England", "2"],
+  
+  ];
